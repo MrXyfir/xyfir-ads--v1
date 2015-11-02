c314652880a05fb3b2e91f45c8368a424df4af62
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-02 12:19:38 -0800

    Created controller for POST api/advertisers/account/register

--- controllers/api/advertisers/account.js
+++ controllers/api/advertisers/account.js
@@ -1,3 +1,4 @@
+var db = require("../../../lib/db");
 module.exports = {
     info: function (req, res) {
     },
@@ -6,6 +7,22 @@ module.exports = {
     addFunds: function (req, res) {
     },
     register: function (req, res) {
+        if (!req.session.uid)
+            res.json({ error: true, message: "You must login to Xyfir Ads with your Xyfir Account." });
+        else if (req.session.advertiser)
+            res.json({ error: true, message: "You are already a advertiser." });
+        else {
+            // Set advertiser boolean to true
+            db(function (connection) {
+                connection.query("UPDATE users SET advertiser = 1 WHERE user_id = ?", [req.session.uid], function (err, result) {
+                    connection.release();
+                    if (err)
+                        res.json({ error: true, message: "An unkown error occured. Please try again." });
+                    else
+                        res.json({ error: false, message: "" });
+                });
+            });
+        }
     }
 };
 //# sourceMappingURL=account.js.map
--- controllers/api/advertisers/account.ts
+++ controllers/api/advertisers/account.ts
@@ -1,4 +1,6 @@
-﻿export = {
+﻿import db = require("../../../lib/db");
+
+export = {
 
     info: (req, res) => {
 
@@ -13,7 +15,23 @@
     },
 
     register: (req, res) => {
+        if (!req.session.uid)
+            res.json({ error: true, message: "You must login to Xyfir Ads with your Xyfir Account." });
+        else if (req.session.advertiser)
+            res.json({ error: true, message: "You are already a advertiser." });
+        else {
+            // Set advertiser boolean to true
+            db(connection => {
+                connection.query("UPDATE users SET advertiser = 1 WHERE user_id = ?", [req.session.uid], (err, result) => {
+                    connection.release();
 
+                    if (err)
+                        res.json({ error: true, message: "An unkown error occured. Please try again." });
+                    else
+                        res.json({ error: false, message: "" });
+                });
+            });
+        }
     }
 
 }
