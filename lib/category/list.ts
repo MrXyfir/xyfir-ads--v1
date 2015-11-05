98d61c75a5282229e307c2d6ba414428dac70e4d
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-05 15:07:33 -0800

    Added placeholder categories list. Added category selection validator for ad and pub campaigns.

---
+++ lib/category/list.ts
@@ -0,0 +1,16 @@
+﻿export = [
+    "Apparel",
+    "Apparel>Accessories",
+    "Apparel>Accessories>Bags & Packs",
+    "Apparel>Accessories>Eyewear",
+    "Apparel>Clothing",
+    "Apparel>Clothing>Children's",
+    "Apparel>Clothing>Costumes",
+    "Entertainment",
+    "Entertainment>TV & Movies",
+    "Entertainment>TV & Movies>Cartoons",
+    "Entertainment>TV & Movies>Comedy",
+    "Entertainment>Music",
+    "Entertainment>Music>Rock",
+    "Entertainment>Music>Hip Hop",
+];
---
+++ lib/category/validator.ts
@@ -0,0 +1,32 @@
+﻿import categories = require("./list");
+
+export = (category: string, isAd: boolean = false): boolean => {
+
+    // Ad cannot have multiple categories (only subcats)
+    if (isAd && !category.match(/^[\w &>]{3,75}$/))
+        return false;
+
+    // Pub campaigns can have multiple categories
+    if (!isAd && !category.match(/^[\w &>,]{3,227}$/))
+        return false;
+
+    category = category.split(',');
+    var match: boolean;
+
+    // Ensure provided categories exist
+    for (var i = 0; i < category.length; i++) {
+        match = false;
+
+        for (var j = 0; j < categories.length; j++) {
+            if (category[i] == categories[j]) {
+                match = true;
+                break;
+            }
+        }
+
+        if (!match) return false;
+    }
+
+    return true;
+
+};
