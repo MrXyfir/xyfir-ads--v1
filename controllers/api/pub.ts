00a4c7d4286c8410f5ac9cb7a4f44e87f7ff81dc
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-06 15:45:37 -0800

    Added controller for GET api/pub/sites for returning all sites in publisher network.

---
+++ controllers/api/pub.ts
@@ -0,0 +1,25 @@
+﻿import db = require("../../lib/db");
+
+export = {
+
+    /*
+        GET api/pub/sites
+        RETURN
+            {sites: [site, site]}
+    */
+    sites: (req, res) => {
+        db(cn => {
+            cn.query("SELECT site FROM pubs", (err, rows) => {
+                cn.release();
+
+                var sites: string[];
+                for (var row in rows) {
+                    sites.push(row.site);
+                }
+
+                res.json({ sites: sites });
+            });;
+        });
+    }
+
+};
