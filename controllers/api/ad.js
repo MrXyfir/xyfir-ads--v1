2b05e4f628ac0f128b4ab6195e71bd98a1cccc6c
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-05 17:01:33 -0800

    Added controller for api/ad/pricing which returns lib/ad/price info object.

--- controllers/api/ad.js
+++ controllers/api/ad.js
@@ -1,5 +1,8 @@
 module.exports = {
     pricing: function (req, res) {
+        require("../../lib/ad/price")(req.query.adType, req.query.payType, req.query.category, function (info) {
+            res.json(info);
+        });
     }
 };
 //# sourceMappingURL=ad.js.map
--- controllers/api/ad.ts
+++ controllers/api/ad.ts
@@ -1,7 +1,10 @@
 export = {
     
     pricing: (req, res) => {
-
+        require("../../lib/ad/price")
+        (req.query.adType, req.query.payType, req.query.category, info => {
+            res.json(info);
+        });
     }
 
 };
