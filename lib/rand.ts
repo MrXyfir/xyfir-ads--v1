55f5acd4774a8c5ae856ec1e115caa00a105ad2a
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-09 15:30:51 -0800

    Added lib module for generating random numbers.

---
+++ lib/rand.ts
@@ -0,0 +1,10 @@
+﻿/*
+    Returns a random integer or floating variable
+    Min is inclusive, max is exclusive
+*/
+export = (min: number, max: number, int: boolean = true): number => {
+    if (int)
+        return Math.floor(Math.random() * (max - min) + min);
+    else
+        return Math.random() * (max - min) + min;
+};
