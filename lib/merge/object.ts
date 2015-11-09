652686d8e0ebfe0a7c6097982fd0a84e67a20aa9
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-09 14:59:01 -0800

    Added lib module for merging objects while adding to existing properties.

--- lib/merge/object.ts
+++ lib/merge/object.ts
@@ -1,3 +1,26 @@
-﻿export = (orginal: any, merge: any) => {
+﻿var mergeObject = (original: any, merge: any) => {
+    for (var prop in merge) {
+        if (merge.hasOwnProperty(prop)) {
 
+            // Original doesn't have property, add it
+            if (original[prop] === undefined) {
+                original[prop] = merge[prop];
+            }
+            // Original has property, add to it
+            else {
+                // Call self recursively for objects
+                if (typeof merge[prop] == "object")
+                    original[prop] = mergeObject(original[prop], merge[prop]);
+                else
+                    original[prop] += merge[prop];
+            }
+        }
+        else {
+            continue;
+        }
+    }
+
+    return original;
 };
+
+export = mergeObject;
