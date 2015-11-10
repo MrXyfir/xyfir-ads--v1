719bfdd37389ec6225e1b40a1cb39a3a789b591f
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-09 16:36:54 -0800

    Added controller for PUT api/advertisers/campaigns/:id for updating certain ad campaign values.

--- controllers/api/advertisers/campaign.ts
+++ controllers/api/advertisers/campaign.ts
@@ -512,11 +512,65 @@ export = {
     /*
         PUT api/advertisers/campaigns/:id
         REQUIRED
-        OPTIONAL
+            name: string, requested: number, available: string, ut_age: string,
+            ct_categories: string, ct_keywords: string, ct_sites: string,
+            ut_countries: string, ut_regions: string, ut_genders: string
         RETURN
-            { error: bool, message: string }
+            { error: boolean, message: string }
+        DESCRIPTION
+            Allows a user to update certain values of their ad campaign
     */
     update: (req, res) => {
+
+        var response: any = { error: false, message: "Campaign updated successfully" };
+
+        /* Validate provided information */
+        if (!req.body.name.match(/^\w{3,25}$/))
+            response = { error: true, message: "Invalid campaign name" };
+        else if (!req.body.available.match(/^(\d{10}-(\d{10})?,?){1,10}$/))
+            response = { error: true, message: "Invalid availability ranges" };
+        else if (!req.body.ut_genders.match(/^[0123,]{1,5}$/))
+            response = { error: true, message: "Invalid targeted user genders" };
+        else if (!req.body.ut_age.match(/^[0123456,]{1,11}$/))
+            response = { error: true, message: "Invalid targeted user age ranges" };
+        else if (!req.body.ct_keywords.match(/^[\w ,]{0,1500}$/))
+            response = { error: true, message: "Invalid keyword(s) length or character" };
+        else if (!req.body.ut_countries.match(/^([A-Z]{2},?){1,50}|\*$/))
+            response = { error: true, message: "Invalid target countries list (limit 50 countries)" };
+        else if (!req.body.ut_regions.match(/^([\w\d\s,\|]{1,1000})|\*$/))
+            response = { error: true, message: "Invalid target regions (limit 1,000 characters)" };
+        else if (!require("../../../lib/category/validator")(req.body.ct_categories, true))
+            response = { error: true, message: "Invalid category selected" };
+        else if (!req.body.ct_sites.match(/^([\w\d.,]){5,225}|\*$/))
+            response = { error: true, message: "Invalid target sites format / length (limit 225 characters)" };
+
+        // Check for errors during validation
+        if (response.error) {
+            res.json(response);
+            return;
+        }
+
+        /* Setup query and values for database */
+        var update: string[] = [
+            req.body.name, req.body.requested, req.body.available, req.body.ut_age,
+            req.body.ct_categories, req.body.ct_keywords, req.body.ct_sites,
+            req.body.ut_countries, req.body.ut_regions, req.body.ut_genders,
+            req.params.id, req.session.uid
+        ];
+        var sql: string = ""
+            + "UPDATE ads SET name = ?, requested = ?, available = ?, ut_age = ?, "
+            + "ct_categories = ?, ct_keywords = ?, ct_sites = ?, "
+            + "ut_countries = ?, ut_regions = ?, ut_genders = ? "
+            + "WHERE id = ? AND owner = ?";
+
+        /* Update data in table */
+        db(cn => cn.query(sql, update, (err, result) => {
+            cn.release();
+
+            if (err) response = { error: true, message: "An unkown error occured" };
+
+            res.json(response);
+        }));
 
     }
 
