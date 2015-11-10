96f43cdd43dd0d03dec77be4c8f042cefe359cfc
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-10 14:23:56 -0800

    Modified info(): Now returns recent payments made from advertiser to add funds to account.

--- controllers/api/advertisers/account.ts
+++ controllers/api/advertisers/account.ts
@@ -96,14 +96,35 @@ export = {
     /*
         GET api/advertisers/account
         RETURN
-            { funds: number }
+            {
+                funds: number, payments: [ { id: string, amount: number, tstamp: datetime } ]
+            }
+        DESCRIPTION
+            Return information relating specifically to advertiser's account
     */
     info: (req, res) => {
         db(cn => {
-            cn.query("SELECT funds FROM advertiser WHERE user_id = ?", [req.session.uid], (err, rows) => {
-                cn.release();
+            var sql: string;
 
-                res.json({ funds: rows[0].funds });
+            // Grab funds in user's account
+            sql = "SELECT funds FROM advertiser WHERE user_id = ?";
+            cn.query(sql, [req.session.uid], (err, rows) => {
+                if (err || rows.length == 0) {
+                    cn.release();
+                    res.json({});
+                    return;
+                }
+
+                var funds: number = rows[0].funds;
+
+                // Grab any payments user has made to add funds to account
+                sql = "SELECT id, amount, tstamp FROM payments WHERE received = ? AND user_id = ? "
+                    + "ORDER BY tstamp DESC";
+                cn.query(sql, [true, req.session.uid], (err, rows) => {
+                    cn.release();
+
+                    res.json({ funds: funds, payments: rows });
+                });
             });
         });
     },
