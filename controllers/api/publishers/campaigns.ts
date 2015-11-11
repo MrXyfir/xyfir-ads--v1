832846c8ad943f5c5fe297d46278cb388418044f
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-11 14:41:46 -0800

    Added controllers for GET and POST api/publishers/campaigns.

--- controllers/api/publishers/campaigns.ts
+++ controllers/api/publishers/campaigns.ts
@@ -1,11 +1,80 @@
-﻿export = {
+﻿import db = require("../../../lib/db");
 
-    getAll: (req, res) => {
+export = {
 
+    /*
+        POST api/publishers/campaigns
+        REQUIRED
+            name: string, categories: string, keywords: string,
+            site: string, type: number
+        RETURN
+            { error: boolean, message: string }
+        DESCRIPTION
+            Creates a new publisher campaign
+    */
+    create: (req, res) => {
+        var response = { error: false, message: "Campaign created successfully" };
+
+        // Validate provided data
+        if (!req.body.match(/^\w{3,25}$/))
+            response = { error: true, message: "Invalid campaign name format or length" };
+        else if (!req.body.keywords.match(/^[\w ,]{0,1599}$/))
+            response = { error: true, message: "Invalid keywords format or length" };
+        else if (!req.body.site.match(/^https?:\/\/[\w\d-.\/]{1,66}$/))
+            response = { error: true, message: "Invalid website format or length" };
+        else if (req.body.type < 0 || req.body.type > 2)
+            response = { error: true, message: "Invalid campaign type" };
+        else if (!require("../../../lib/category/validator")(req.body.categories))
+            response = { error: true, message: "Invalid categories provided" };
+
+        // Check for error during validation
+        if (response.error) {
+            res.json(response);
+            return;
+        }
+
+        var sql: string = "INSERT INTO pubs SET ?";
+        req.body.owner = req.session.uid;
+
+        // Insert campaign into database
+        db(cn => cn.query(sql, req.body, (err, result) => {
+
+            if (err || !result.insertId) {
+                cn.release();
+                res.json({ error: true, message: "An unkown error occured" });
+                return;
+            }
+
+            // Create blank report in pub_reports
+            sql = "INSERT INTO pub_reports (id, day) VALUES (?, CURDATE())";
+            cn.query(sql, [result.insertId], (err, result) => {
+                cn.release();
+
+                res.json(response);
+            });
+        }));
     },
 
-    create: (req, res) => {
+    /*
+        GET api/publishers/campaigns
+        RETURN
+            {campaigns: [
+                { id: number, name: string, site: string, type: number }
+            ]}
+        DESCRIPTION
+            Returns basic information for all of publisher's campaigns
+    */
+    getAll: (req, res) => {
+        var sql: string = "SELECT id, name, site, type FROM publishers WHERE owner = ?";
 
+        db(cn => cn.query(sql, [req.session.uid], (err, rows) => {
+            cn.release();
+
+            if (err || rows.length == 0)
+                res.json({ campaigns: [] });
+            else
+                res.json({ campaigns: rows });
+        }));
     }
 
 };
