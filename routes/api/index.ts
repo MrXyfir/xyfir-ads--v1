055c7d42ef5c47de1514de43bddd64bd9ef446f5
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-12 13:19:41 -0800

    Added routes for api/xad-id/**.

--- routes/api/index.ts
+++ routes/api/index.ts
@@ -5,6 +5,7 @@ router.use('/pub', require('./pub'));
 router.use('/ads', require('./ads'));
 router.use('/login', require('./login'));
 router.use('/panel', require('./panel/'));
+router.use('/xad-id', require('./xad-id'));
 //router.use('/upload', require('./upload'));
 router.use('/publishers', require('./publishers/'));
 router.use('/advertisers', require('./advertisers/'));
---
+++ routes/api/xad-id.ts
@@ -0,0 +1,9 @@
+﻿var router = require('express').Router();
+var xad = require('../../controllers/api/xad-id');
+
+router.route('/:xacc/:xad')
+    .get(xad.info)
+    .put(xad.update);
+router.post('/:xacc', xad.create);
+
+export = router;
