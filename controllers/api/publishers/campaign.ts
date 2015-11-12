3ff4687c129df54a3416646bd8876af8fa88b287
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-11 16:10:07 -0800

    Added controller for PUT api/publishers/campaigns/:id.

--- controllers/api/publishers/campaign.ts
+++ controllers/api/publishers/campaign.ts
@@ -85,6 +85,53 @@ export = {
     },
 
     /*
+        PUT api/publishers/campaigns/:id
+        REQUIRED
+            name: string, categories: string, keywords: string,
+            site: string, type: number
+        RETURN
+            { error: boolean, message: string }
+        DESCRIPTION
+            Allows user to update various campaign values
+    */
+    update: (req, res) => {
+        var response: any = { error: false, message: "Campaign updated successfully" };
+
+        if (!req.body.name.match(/^\w{3,25}$/))
+            response = { error: true, message: "Invalid campaign name format or length" };
+        else if (!req.body.keywords.match(/^[\w ,]{0,1599}$/))
+            response = { error: true, message: "Invalid keywords format or length" };
+        else if (!req.body.site.match(/^https?:\/\/[\w\d-.\/]{1,66}$/))
+            response = { error: true, message: "Invalid website format or length" };
+        else if (req.body.type < 0 || req.body.type > 2)
+            response = { error: true, message: "Invalid campaign type" };
+        else if (!require("../../../lib/category/validator")(req.body.categories))
+            response = { error: true, message: "Invalid categories provided" };
+
+        if (response.error) {
+            res.json(response);
+            return;
+        }
+
+        var sql: string = "UPDATE pubs "
+            + "SET name = ?, keywords = ?, site = ?, type = ?, categories = ? "
+            + "WHERE id = ? AND owner = ?";
+        var update: any[] = [
+            req.body.name, req.body.keywords, req.body.site, req.body.type,
+            req.body.categories, req.params.id, req.session.uid
+        ];
+
+        db(cn => cn.query(sql, update, (err, result) => {
+            cn.release();
+
+            if (err)
+                res.json({ error: true, message: "An unknown error occured" });
+            else
+                res.json(response);
+        }));
+    },
+
+    /*
         GET api/publishers/campaigns/:id/reports
         REQUIRED
             dates: "2015-07-20|2015-07-20:2015-07-30"
@@ -97,20 +144,6 @@ export = {
             Generates a report for a campaign over a specific time frame
     */
     reports: (req, res) => {
-
-    },
-
-    /*
-        PUT api/publishers/campaigns/:id
-        REQUIRED
-            name: string, categories: string, keywords: string,
-            site: string, type: number
-        RETURN
-            { error: boolean, message: string }
-        DESCRIPTION
-            Allows user to update various campaign values
-    */
-    update: (req, res) => {
 
     }
 
