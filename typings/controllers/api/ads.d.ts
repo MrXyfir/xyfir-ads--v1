c3a5d6105c62fdb109e0733ab801d26fd0a78064
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-13 15:42:05 -0800

    Added type definitions for controllers/api/ads.ts.

---
+++ typings/controllers/api/ads.d.ts
@@ -0,0 +1,26 @@
+﻿interface IAd {
+    type: number, link: string, title: string,
+    description: string, media?: string,
+    score?: number
+}
+
+interface IPub {
+    type: number, categories: string, keywords: string, site: string
+}
+
+interface IRequestQuery {
+    pubid: number, xadid?: string, type?: number, types?: string,
+    count?: number, speed?: number, ip?: string, age?: number,
+    gender?: number, categories?: string, keywords?: string
+}
+
+interface IUser {
+    country: string, region: string, age?: number, gender?: number
+}
+
+interface IAdsRow {
+    pay_type: number, cost: number, available: string, ad_type: number,
+    ad_title: string, ad_description: string, ad_link: string,
+    ad_media: string, ut_age: string, ut_countries: string, ct_keywords: string,
+    ut_regions: string, ut_gender: string, ct_categories: string
+}
