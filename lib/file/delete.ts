854083b15f5552246bc3089530c73ef5c36092aa
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-17 14:50:27 -0800

    Now deletes files by id from Cloudinary.

--- lib/file/delete.ts
+++ lib/file/delete.ts
@@ -1,4 +1,11 @@
-﻿// Deletes a file by id from Cloudinary
+﻿var cloudinary = require("cloudinary");
+var config = require("../../config").cloudinary;
+
+// Deletes a file by id from Cloudinary
 export = (id: string): void => {
+
+    cloudinary.config(config);
+
+    cloudinary.api.delete_resources([id], res => { return; });
 
 };
