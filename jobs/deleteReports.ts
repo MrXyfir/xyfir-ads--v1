e13320c7f29d6254ed0d6952ae3151fb8a248dc7
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-18 14:16:33 -0800

    Added job module for deleting reports over 3 months old.

---
+++ jobs/deleteReports.ts
@@ -0,0 +1,19 @@
+﻿import db = require("../lib/db");
+
+/*
+    Delete ad/pub reports older than 3 months
+*/
+export = db(cn => {
+
+    var sql: string = "DELETE FROM ad_reports WHERE day < DATE_SUB(CURDATE(), INTERVAL 3 MONTH)";
+    cn.query(sql, (err, result) => {
+        if (err) {
+            cn.release();
+            return;
+        }
+
+        sql = "DELETE FROM pub_reports WHERE day < DATE_SUB(CURDATE(), INTERVAL 3 MONTH)";
+        cn.query(sql, (err, result) => cn.release());
+    });
+
+});
