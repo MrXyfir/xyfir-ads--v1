9b156489d98233a9e8afdfd1d8740101136fab4e
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-18 14:43:04 -0800

    Added job module for updating all ad campaign's bid price where autobid == true.

---
+++ jobs/autobid.ts
@@ -0,0 +1,33 @@
+﻿import autobid = require("../lib/ad/autobid");
+import db = require("../lib/db");
+
+/*
+    Generate a new bid for all ad campaigns with autobid
+*/
+export = (fn: any): void => db(cn => {
+
+    // Get id of all ads with autobid enabled
+    cn.query("SELECT id FROM ads WHERE autobid = 1 AND approved = 1")
+    .on("result", handleRow)
+    .on("error", onError)
+    .on("end", () => {
+        cn.release();
+        fn(false);
+    });
+
+    var handleRow = (row): void => {
+        cn.pause();
+
+        autobid(row.id, cn, err => {
+            if (err) onError(true);
+            else cn.resume();
+        });
+    };
+
+    var onError = (err: any): void => {
+        cn.end();
+        cn.release();
+        fn(true);
+    };
+
+});
