39d917e83065ff39ac5361c4d76b58fd266a65cd
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-18 14:22:30 -0800

    Added job module for subtracting daily_funds_used from funds and resetting daily_funds_used on ads where daily_funds > 0.

---
+++ jobs/dailyAllocatedFunds.ts
@@ -0,0 +1,16 @@
+﻿import db = require("../lib/db");
+
+/*
+    Subtract daily_funds_used from funds for ads where daily_funds > 0
+    Reset daily_funds_used to 0
+*/
+export = (fn: any): void => db(cn => {
+
+    var sql = "UPDATE ads SET funds = funds - daily_funds_used, daily_funds_used = 0 "
+        + "WHERE daily_funds > 0";
+    cn.query(sql, (err, result) => {
+        cn.release();
+        fn(!!err);
+    });
+
+});
