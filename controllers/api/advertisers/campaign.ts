e6cb5dcf0edc5314cd6a863ab445b05fdfa08a2d
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-22 15:46:03 -0800

    Fixed errors in remove().

--- controllers/api/advertisers/campaign.ts
+++ controllers/api/advertisers/campaign.ts
@@ -111,12 +111,12 @@ export = {
                 sql = "INSERT INTO ads_ended SELECT "
                     + "id, name, pay_type, cost, autobid, available, approved, ad_type, ad_title, "
                     + "ad_description, ad_link, ad_media, ut_age, ut_countries, ut_regions, "
-                    + "ut_genders, ct_categories, ct_keywords, ct_sites, info, owner "
-                    + "FROM ads WHERE id = ?";
+                    + "ut_genders, ct_categories, ct_keywords, ct_sites, owner "
+                    + "FROM ads WHERE id = ? AND owner = ?";
                 cn.query(sql, [req.params.id, req.session.uid], (err, result) => {
                     if (err) {
                         cn.rollback(() => cn.release());
-                        res.json({ error: true, message: "An unkown error occured." });
+                        res.json({ error: true, message: "An unkown error occured.-" });
                         return;
                     }
 
@@ -125,23 +125,23 @@ export = {
                     cn.query(sql, [req.params.id, req.session.uid], (err, result) => {
                         if (err) {
                             cn.rollback(() => cn.release());
-                            res.json({ error: true, message: "An unkown error occured." });
+                            res.json({ error: true, message: "An unkown error occured.--" });
                             return;
                         }
 
                         // Delete all rows relating to ad in clicks table
-                        sql = "DELETE FROM clicks WHERE ad_id = ? AND owner = ?";
-                        cn.query(sql, [req.params.id, req.session.uid], (err, result) => {
+                        sql = "DELETE FROM clicks WHERE ad_id = ?";
+                        cn.query(sql, [req.params.id], (err, result) => {
                             if (err) {
                                 cn.rollback(() => cn.release());
-                                res.json({ error: true, message: "An unkown error occured." });
+                                res.json({ error: true, message: "An unkown error occured.---" });
                                 return;
                             }
 
                             cn.commit(err => {
                                 if (err) {
                                     cn.rollback(() => cn.release());
-                                    res.json({ error: true, message: "An unkown error occured." });
+                                    res.json({ error: true, message: "An unkown error occured.----" });
                                     return;
                                 }
 
