93023c43567bb2eaccb0909fbb54c38f746189b6
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-11-25 16:25:18 -0800

    Added controller/route description comment.

--- controllers/api/ad.ts
+++ controllers/api/ad.ts
@@ -1,5 +1,14 @@
 export = {
-    
+
+    /*
+        GET api/ad/pricing
+        REQUIRED
+            adType: number, payType: number, category: string
+        RETURN
+            { base: number, competitors: number, average: number, highest: number }
+        DESCRIPTION
+            Returns info object from lib/ad/price module
+    */
     pricing: (req, res) => {
         require("../../lib/ad/price")
         (req.query.adType, req.query.payType, req.query.category, info => {
