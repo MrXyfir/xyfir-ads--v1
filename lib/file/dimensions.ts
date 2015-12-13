df4ac3716018846aa675e8d9296e4f069e1bc813
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-12-13 09:55:08 -0800

    Added proper file dimensions and names for image/video ads.

--- lib/file/dimensions.ts
+++ lib/file/dimensions.ts
@@ -1,12 +1,14 @@
-﻿// ** Finish / improve file dimensions
-export = {
+﻿export = {
     video: [
-        { width: 0, height: 360, size: 0 },
-        { width: 0, height: 480, size: 1 },
-        { width: 0, height: 720, size: 2 },
-        { width: 0, height: 1080, size: 3 }
+        { width: 256, height: 144,  size: 0, quality: "Mobile" },
+        { width: 480, height: 360,  size: 1, quality: "Low" },
+        { width: 720, height: 480,  size: 2, quality: "Medium" },
+        { width: 1080, height: 720, size: 3, quality: "High" }
     ],
     image: [
-        { width: 0, height: 0, size: 0 }
+        { width: 468, height: 60,  size: 0, name: "Banner" },
+        { width: 250, height: 250, size: 1, name: "Square" },
+        { width: 300, height: 250, size: 2, name: "Rectangle" },
+        { width: 120, height: 600, size: 3, name: "Skyscraper" }
     ]
 };
