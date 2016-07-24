d7c09d6c2affe7cbf5e87c3856dfbd1d762eaae3
Mr. Xyfir <mr@xyfir.com>
2016-07-24 16:23:21 -0700

    Remove typings

--- typings/jobs/validateClicks.d.ts
+++
@@ -1,5 +0,0 @@
-﻿interface IClick {
-    ad_id: number, pub_id: number, xad_id: string, cost: number,
-    ip: string, clicked: number, served: number, user?: number,
-    signature: string, valid?: boolean
-}
