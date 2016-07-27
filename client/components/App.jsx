60bad9018923f632847fc6fb925dc076433b333e
Mr. Xyfir <mr@xyfir.com>
2016-07-27 16:18:26 -0700

    Implement login system

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,18 +7,67 @@ import Publishers from "./containers/Publishers";
 import Panel from "./containers/Panel";
 import Home from "./containers/Home";
 
+// Modules
+import parseHashQuery from "lib/parse-hash-query";
+import request from "lib/request";
+
+// Constants
+import { XACC } from "constants/config";
+
 class App extends React.Component {
 	
     constructor(props) {
         super(props);
 
         this.state = {
-            hash: location.hash.split('/')
+            hash: location.hash.split('?')[0].split('/'),
+            loading: true
         };
 
         window.onhashchange = () => {
-            this.setState({ hash: location.hash.split('/') });
+            this.setState({
+                hash: location.hash.split('?')[0].split('/')
+            });
         };
+
+        this._login = this._login.bind(this);
+
+        this._login();
+    }
+
+    _login() {
+        const q = parseHashQuery();
+
+        // Attempt to login using XID/AUTH or skip to initialize()
+        if (q.xid && q.auth) {
+            request({
+                url: "api/account/login",
+                method: "POST",
+                data: { xid: q.xid, auth: q.auth },
+                success: (res) => {
+                    if (res.error) {
+                        location.href = XACC + "login/11";
+                    }
+                    else {
+                        // Clear ?xid=...&auth=... from hash
+                        this.setState({ loading: false });
+                        location.hash = location.hash.split('?')[0];
+                    }
+                }
+            });
+        }
+        // User must be logged in if anywhere but home page
+        else if (location.hash) {
+            request({
+                url: "api/account/status",
+                success: (res) => {
+                    if (!res.loggedIn)
+                        location.href = XACC + "login/11";
+                    else
+                        this.setState({ loading: false });
+                }
+            });
+        }
     }
 	
 	render() {
