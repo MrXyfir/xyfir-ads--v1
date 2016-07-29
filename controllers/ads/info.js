ef2888bf4f938c1da327b8896400d9e77412c25d
Mr. Xyfir <mr@xyfir.com>
2016-07-29 16:12:16 -0700

    Fix sql

--- controllers/ads/info.js
+++ controllers/ads/info.js
@@ -11,7 +11,7 @@ const db = require("lib/db");
 */
 module.exports = function(req, res) {
         
-        const sql = "SELECT ad_titile FROM ads WHERE id = ?";
+        const sql = "SELECT ad_title FROM ads WHERE id = ?";
 
         db(cn => cn.query(sql, [req.query.id], (err, rows) => {
             cn.release();
