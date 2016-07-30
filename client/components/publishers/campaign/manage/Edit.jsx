a11c7f0b3a36fcf59f11d32a4ed4f649d13b8f92
Mr. Xyfir <mr@xyfir.com>
2016-07-30 14:20:49 -0700

    h3 -> label

--- client/components/publishers/campaign/manage/Edit.jsx
+++ client/components/publishers/campaign/manage/Edit.jsx
@@ -63,16 +63,16 @@ export default class EditPublisherCampaign extends React.Component {
             <div className="campaign-edit">
                 {alert}
 
-                <h3>Campaign Name</h3>
+                <label>Campaign Name</label>
                 <input type="text" ref="name" defaultValue={c.name} />
 
-                <h3>Site</h3>
+                <label>Site</label>
                 <input type="text" ref="site" defaultValue={c.site} />
 
-                <h3>Keywords</h3>
+                <label>Keywords</label>
                 <textarea ref="keywords" defaultValue={c.keywords} />
 
-                <h3>Type</h3>
+                <label>Type</label>
                 <select ref="type" defaultValue={c.type}>
                     <option value="1">Website</option>
                     <option value="2">App / Web App</option>
@@ -82,7 +82,7 @@ export default class EditPublisherCampaign extends React.Component {
 
                 <hr />
 
-                <h3>Test Key</h3>
+                <label>Test Key</label>
                 <input type="text" onFocus={(e) => e.target.select()} value={c.test} />
                 <Button type="secondary" onClick={() => this.onGenerateTestKey}>
                     Generate New Key
