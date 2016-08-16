31d7d4c6415f519d18f11891ba84ad3dbe079e00
MrXyfir <mr@xyfir.com>
2016-08-15 17:53:44 -0700

    Replace ads_ended table with ended column in ads table

--- controllers/panel/awaiting/ads/deny.js
+++ controllers/panel/awaiting/ads/deny.js
@@ -9,7 +9,7 @@ const db = require("lib/db");
         { error: boolean }
     DESCRIPTION
         Refunds advertiser 10% of funds
-        Moves advertisement to ads_ended
+        Mark ad as ended
         Deletes advertisement from ads table
         Notifies advertiser of denial via email
         Deletes media from Cloudinary if available
@@ -43,12 +43,8 @@ module.exports = function(req, res) {
                     return;
                 }
 
-                // Move relevant data from ads -> ads_ended
-                sql = "INSERT INTO ads_ended SELECT "
-                    + "id, name, pay_type, cost, autobid, available, approved, ad_type, ad_title, "
-                    + "ad_description, ad_link, ad_media, ut_age, ut_countries, ut_regions, "
-                    + "ut_genders, ct_categories, ct_keywords, ct_sites, owner "
-                    + "FROM ads WHERE id = ?";
+                // Mark ad as ended
+                sql = "UPDATE ads SET ended = 1, funds = 0 WHERE id = ?"; 
                 cn.query(sql, [req.params.id], (e, r) => {
                     if (e || !r.affectedRows) {
                         cn.rollback(() => cn.release());
@@ -56,61 +52,51 @@ module.exports = function(req, res) {
                         return;
                     }
 
-                    // Delete row from ads
-                    sql = "DELETE FROM ads WHERE id = ?";
-                    cn.query(sql, [req.params.id], (e, r) => {
-                        if (e || !r.affectedRows) {
+                    cn.commit(err => {
+                        if (err) {
                             cn.rollback(() => cn.release());
                             res.json({ error: true });
                             return;
                         }
 
-                        cn.commit(err => {
-                            if (err) {
-                                cn.rollback(() => cn.release());
-                                res.json({ error: true });
-                                return;
-                            }
+                        let uEmail = "";
 
-                            let uEmail = "";
+                        let finish = () => {
+                            // Send denial email to advertiser
+                            email(uEmail, "Advertising Campaign - Denied",
+                                "Your campaign was denied for the following reason: " + req.body.reason
+                            );
+                            res.json({ error: false });
 
-                            let finish = () => {
-                                // Send denial email to advertiser
-                                email(uEmail, "Advertising Campaign - Denied",
-                                    "Your campaign was denied for the following reason: " + req.body.reason
-                                );
-                                res.json({ error: false });
+                            // Check if we need to delete content from Cloudinary
+                            if (!!media[0]) {
+                                let ids = [], temp;
 
-                                // Check if we need to delete content from Cloudinary
-                                if (!!media[0]) {
-                                    let ids = [], temp;
+                                for (let i = 0; i < media.length; i++) {
+                                    // Grab id + .ext
+                                    temp = media[i].substr(media[i].lastIndexOf('/') + 1);
+                                    // Cut off .ext and leave id
+                                    temp = temp.substr(0, temp.length - 4);
+                                    ids.push(temp);
+                                }
 
-                                    for (let i = 0; i < media.length; i++) {
-                                        // Grab id + .ext
-                                        temp = media[i].substr(media[i].lastIndexOf('/') + 1);
-                                        // Cut off .ext and leave id
-                                        temp = temp.substr(0, temp.length - 4);
-                                        ids.push(temp);
-                                    }
-
-                                    require("lib/file/delete")(ids);
-                                };
+                                require("lib/file/delete")(ids);
                             };
+                        };
 
-                            sql = "SELECT email FROM users WHERE user_id = ?";
-                            cn.query(sql, [owner], (err, rows) => {
-                                if (err || !rows.length) {
-                                    cn.release();
-                                    res.json({ error: true });
-                                }
-                                else {
-                                    uEmail = rows[0].email;
-                                    finish();
-                                }
-                            }); // get user's email
-                        }); // commit transaction
-                    }); // delete from ads
-                }); // move to ads_ended
+                        sql = "SELECT email FROM users WHERE user_id = ?";
+                        cn.query(sql, [owner], (err, rows) => {
+                            if (err || !rows.length) {
+                                cn.release();
+                                res.json({ error: true });
+                            }
+                            else {
+                                uEmail = rows[0].email;
+                                finish();
+                            }
+                        }); // get user's email
+                    }); // commit transaction
+                }); // mark as ended
             }); // refund funds
         }); // grab ad data
     }));
