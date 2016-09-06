b10549228ed51719135ff673914f08e2486faa95
MrXyfir <mr@xyfir.com>
2016-09-06 10:01:03 -0700

    Update / improve code. Modify to accept POST instead of GET + redirect

--- controllers/account/login.js
+++ controllers/account/login.js
@@ -1,49 +1,60 @@
 /*
-	GET api/login
+	POST api/login
 	REQUIRED
         xid: string, auth: string
 	RETURN
-		{error: bool, message: string}
+		{ error: bool }
     DESCRIPTION
-        Successfull logins to Xyfir Account redirect here
-	    Set session if xid/auth is valid
-        Redirects to home page
+        Attempt to login / register user
 */
 
 const request = require("request");
 const db = require("lib/db");
 
-let config = require("config");
+const config = require("config");
 
 module.exports = function(req, res) {
 
-    if (req.session.uid) {
-        res.redirect(config.addresses.xads);
+    // User already logged in
+    if (!!req.session.uid) {
+        res.json({ error: false });
         return;
     }
 
     let url = config.addresses.xacc
         + "api/service/11/" + config.keys.xacc
-        + "/" + req.query.xid
-        + "/" + req.query.auth;
+        + "/" + req.body.xid
+        + "/" + req.body.auth;
 
 
     request(url, (err, response, body) => {
+        // Error with actual request
+        if (err) {
+            res.json({ error: true });
+            return;
+        }
+
         body = JSON.parse(body);
 
+        // Error from Xyfir Accounts
+        // Most likely invalid auth/xid combo
         if (body.error) {
-            res.redirect(config.addresses.xads);
+            res.json({ error: true });
             return;
         }
 
         db(cn => {
             let sql = "SELECT * FROM users WHERE xyfir_id = ?";
-            cn.query(sql, [req.query.xid], (err, rows) => {
+            cn.query(sql, [req.body.xid], (err, rows) => {
 				
-                // Check if first login (registration) or normal login
-                if (rows.length == 0) {
+                if (err) {
+                    cn.release();
+                    res.json({ error: true });
+                }
+                // First login (registration)
+                else if (rows.length == 0) {
                     let insert = {
-                        xyfir_id: req.query.xid,
+                        xyfir_id: req.body.xid,
                         email: body.email
                     };
 					
@@ -53,21 +64,22 @@ module.exports = function(req, res) {
                         cn.release();
 
                         req.session.uid = result.insertId;
-                        res.redirect(config.addresses.xads);
+                        res.json({ error: false });
                     });
                 }
+                // Normal login
                 else {
                     // Update user data
                     sql = "UPDATE users SET email = ? WHERE user_id = ?";
                     cn.query(sql, [body.email, rows[0].user_id], (err, result) => {
                         cn.release();
 
-                        // Set session letiables
+                        // Set session variables
                         req.session.uid = rows[0].user_id;
                         req.session.publisher = !!rows[0].publisher;
                         req.session.advertiser = !!rows[0].advertiser;
 
-                        res.redirect(config.addresses.xads);
+                        res.json({ error: false });
                     });
                 }
             });
