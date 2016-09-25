b6d4b988d282ed233a410595f014b661ca6b3ed8
MrXyfir <mr@xyfir.com>
2016-09-25 14:43:13 -0700

    Update / improve code

--- controllers/publishers/campaigns/reports.js
+++ controllers/publishers/campaigns/reports.js
@@ -4,7 +4,9 @@ const db = require("lib/db");
 /*
     GET api/publishers/campaigns/:id/reports
     REQUIRED
-        dates: "2015-07-20|2015-07-20:2015-07-30"
+        dates: string
+            YYYY-MM-DD
+            YYYY-MM-DD:YYYY-MM-DD
     RETURN
         {
             clicks: number, views: number, earnings: number,
@@ -15,62 +17,46 @@ const db = require("lib/db");
 */
 module.exports = function(req, res) {
     
+    let dates = req.query.dates.split(':');
+    let sql = `
+        SELECT clicks, views, earnings, earnings_temp as pending, ads
+        FROM pub_reports WHERE id = ? AND day BETWEEN ? AND ?
+    `;
+
+    // Single date to date-range (both same day)
+    if (dates.length == 1) dates[1] = dates[0];
+
     db(cn => {
-        let sql;
+        let report = {
+            clicks: 0, views: 0, earnings: 0, pending: "", ads: ""
+        };
+        let query = cn.query(sql, [req.params.id, dates[0], dates[1]]);
 
-        // Generate a report for a single day
-        if (req.query.dates.match(/^\d{4}-\d{2}-\d{2}$/)) {
-
-            sql = "SELECT clicks, views, earnings, earnings_temp as pending, ads "
-            + "FROM pub_reports WHERE id = ? AND day = ?";
-            cn.query(sql, [req.params.id, req.query.dates], (err, rows) => {
-                cn.release();
-                res.json(rows[0]);
-            });
-        }
-
-        // Generate a report over multiple days
-        else if (req.query.dates.match(/^(\d{4}-\d{2}-\d{2}:?){2}$/)) {
-            // Setup letiables
-            let dates = req.query.dates.split(':');
-            let report = {
-                clicks: 0, views: 0, earnings: 0, pending: "", ads: ""
-            };
-
-            sql = "SELECT clicks, views, earnings, earnings_temp as pending, ads "
-            + "FROM pub_reports WHERE id = ? AND day BETWEEN ? AND ? ";
-            let query = cn.query(sql, [req.params.id, dates[0], dates[1]]);
-
-            query // Loop through each row
-                .on("error", err => {
-                    cn.end();
-                    cn.release();
-                    res.json({});
-                    return;
-                })
-                .on("result", row => {
-                    cn.pause();
-                
-                    // Add values of new row to total
-                    report.earnings += row.earnings;
-                    report.pending += row.pending;
-                    report.clicks += row.clicks;
-                    report.views += row.views;
-
-                    // Merge top ad campaign lists
-                    report.ads = mergeList(report.ads.split(','), row.ads.split(','));
-
-                    cn.resume();
-                })
-                .on("end", () => {
-                    cn.release();
-                    res.json(report);
-                });
-        }
-
-        else {
+        query // Loop through each row
+        .on("error", err => {
+            cn.end();
+            cn.release();
             res.json({});
-        }
+            return;
+        })
+        .on("result", row => {
+            cn.pause();
+        
+            // Add values of new row to total
+            report.earnings += row.earnings;
+            report.pending += row.pending;
+            report.clicks += row.clicks;
+            report.views += row.views;
+
+            // Merge top ad campaign lists
+            report.ads = mergeList(report.ads.split(','), row.ads.split(','));
+
+            cn.resume();
+        })
+        .on("end", () => {
+            cn.release();
+            res.json(report);
+        });
     });
 
 }
