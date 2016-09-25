13b039bd4229e7e268de3391f060e81e702cae16
MrXyfir <mr@xyfir.com>
2016-09-25 14:30:28 -0700

    Clean up / improve code

--- controllers/advertisers/campaigns/reports.js
+++ controllers/advertisers/campaigns/reports.js
@@ -1,9 +1,13 @@
+const mergeObject = require("lib/merge/object");
+const mergeList = require("lib/merge/list");
 const db = require("lib/db");
 
 /*
     GET api/advertisers/campaigns/:id/reports
     REQUIRED
-        dates: "2015-07-20|2015-07-20:2015-07-30"
+        dates: string
+            YYYY-MM-DD
+            YYYY-MM-DD:YYYY-MM-DD
     RETURN
         {
             clicks: number, views: number, cost: number, publishers: string,
@@ -13,62 +17,48 @@ const db = require("lib/db");
         Generates a report for a campaign over a specific time frame
 */
 module.exports = function(req, res) {
+
+    let dates = req.query.dates.split(':');
+    let sql = "SELECT * FROM ad_reports WHERE id = ? AND day BETWEEN ? AND ?";
+
+    // Single date to date-range (both same day)
+    if (dates.length == 1) dates[1] = dates[0];
     
     db(cn => {
-        let sql;
+        let report = {
+            clicks: 0, views: 0, cost: 0, publishers: "",
+            dem_age: "", dem_gender: "", dem_geo: {}
+        };
+        
+        let query = cn.query(sql, [req.params.id, dates[0], dates[1]]);
 
-        // Generate a report for a single day
-        if (req.query.dates.match(/^\d{4}-\d{2}-\d{2}$/)) {
+        query
+        .on("error", err => {
+            cn.end();
+            cn.release();
+            res.json({});
+            return;
+        })
+        .on("result", row => {
+            cn.pause();
+            
+            // Add values of new row to total
+            report.clicks += row.clicks;
+            report.views += row.views;
+            report.cost += row.cost;
 
-            sql = "SELECT * FROM ad_reports WHERE id = ? AND day = ?";
-            cn.query(sql, [req.params.id, req.query.dates], (err, rows) => {
-                cn.release();
-                res.json(rows[0]);
-            });
-        }
+            // Merge lists / objects
+            report.publishers = mergeList(report.publishers.split(','), row.publishers.split(','));
+            report.dem_gender = mergeList(report.dem_gender.split(','), row.dem_gender.split(','));
+            report.dem_age = mergeList(report.dem_age.split(','), row.dem_age.split(','));
+            report.dem_geo = mergeObject(report.dem_geo, JSON.parse(row.dem_geo));
 
-        // Generate a report over multiple days
-        else if (req.query.dates.match(/^(\d{4}-\d{2}-\d{2}:?){2}$/)) {
-            // Setup letiables
-            let dates = req.query.dates.split(':');
-            let report = {
-                clicks: 0, views: 0, cost: 0, publishers: "",
-                dem_age: "", dem_gender: "", dem_geo: {}
-            };
-
-            sql = "SELECT * FROM ad_reports WHERE id = ? AND day BETWEEN ? AND ?";
-            let query = cn.query(sql, [req.params.id, dates[0], dates[1]]);
-            let mergeList = require("lib/merge/list");
-            let mergeObject = require("lib/merge/object");
-
-            query
-            .on("error", err => {
-                cn.end();
-                cn.release();
-                res.json({});
-                return;
-            })
-            .on("result", row => {
-                cn.pause();
-                
-                // Add values of new row to total
-                report.clicks += row.clicks;
-                report.views += row.views;
-                report.cost += row.cost;
-
-                // Merge lists / objects
-                report.publishers = mergeList(report.publishers.split(','), row.publishers.split(','));
-                report.dem_gender = mergeList(report.dem_gender.split(','), row.dem_gender.split(','));
-                report.dem_age = mergeList(report.dem_age.split(','), row.dem_age.split(','));
-                report.dem_geo = mergeObject(report.dem_geo, JSON.parse(row.dem_geo));
-
-                cn.resume();
-            })
-            .on("end", () => {
-                cn.release();
-                res.json(report);
-            });
-        }
+            cn.resume();
+        })
+        .on("end", () => {
+            cn.release();
+            res.json(report);
+        });
     });
 
 }
