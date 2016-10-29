37d0a2d025c10348890cf3634f87c56a21b87587
MrXyfir <mr@xyfir.com>
2016-10-29 12:45:45 -0700

    Use ';' to separate commands. Improve console output

--- jobs/updateGeoIpDatabase.js
+++ jobs/updateGeoIpDatabase.js
@@ -3,12 +3,12 @@
 module.exports = () => {
     // Move into geoip-lite's directory
     // Run npm script to update the database
-    const cmd = "cd node_modules/geoip-lite & npm run-script updatedb";
+    const cmd = "cd node_modules/geoip-lite; npm run-script updatedb";
 
     exec(cmd, (error, stdout, stderr) => {
-        console.log('running/ended');
-        console.log(error);
-        console.log(stdout);
-        console.log(stderr);
+        console.log("Update geoip db");
+        console.log("Error", error);
+        console.log("Output", stdout);
+        console.log("Error Ouput", stderr);
     });
 };
