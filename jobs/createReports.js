373e452098248bd9e23d23ddaaa4362aca1ca145
MrXyfir <mr@xyfir.com>
2016-10-29 12:44:28 -0700

    Clean up SQL

--- jobs/createReports.js
+++ jobs/createReports.js
@@ -7,15 +7,15 @@ module.exports = (fn) => db(cn => {
     
     let sql = "";
 
-        // Insert into ad_reports with id and day
-    sql = "INSERT INTO ad_reports (id, day) "
-        // Where ad in ads is approved AND ...
-        + "SELECT id, DATE_ADD(CURDATE(), INTERVAL 1 DAY) FROM ads "
-        + "WHERE approved = 1 AND ended = 0 AND NOT EXISTS "
-        // Where report DOES NOT exists with id/day
-        + "(SELECT * FROM ad_reports WHERE ad_reports.id = ads.id AND "
-        + "ad_reports.day = DATE_ADD(CURDATE(), INTERVAL 1 DAY))";
-
+    // Insert into ad_reports with id and day
+    sql = `
+        INSERT INTO ad_reports (id, day)
+        SELECT id, DATE_ADD(CURDATE(), INTERVAL 1 DAY) FROM ads
+        WHERE approved = 1 AND ended = 0 AND NOT EXISTS (
+            SELECT * FROM ad_reports WHERE ad_reports.id = ads.id
+            AND ad_reports.day = DATE_ADD(CURDATE(), INTERVAL 1 DAY)
+        )
+    `;
     cn.query(sql, (err, result) => {
         if (err) {
             cn.release();
@@ -23,13 +23,15 @@ module.exports = (fn) => db(cn => {
             return;
         }
         
-            // Insert into pub_reports with id and day
-        sql = "INSERT INTO pub_reports (id, day) "
-            // Where pub from pubs DOES NOT have a report for day
-            + "SELECT id, DATE_ADD(CURDATE(), INTERVAL 1 DAY) FROM pubs "
-            + "WHERE NOT EXISTS (SELECT * FROM pub_reports WHERE "
-            + "pub_reports.id = pubs.id AND "
-            + "pub_reports.day = DATE_ADD(CURDATE(), INTERVAL 1 DAY))";
+        // Insert into pub_reports with id and day
+        sql = `
+            INSERT INTO pub_reports (id, day)
+            SELECT id, DATE_ADD(CURDATE(), INTERVAL 1 DAY) FROM pubs
+            WHERE NOT EXISTS (
+                SELECT * FROM pub_reports WHERE pub_reports.id = pubs.id
+                AND pub_reports.day = DATE_ADD(CURDATE(), INTERVAL 1 DAY)
+            )
+        `;
         cn.query(sql, (err, result) => {
             cn.release();
             
