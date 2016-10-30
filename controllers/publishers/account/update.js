60bbc4bdea5050f9f356dba0a5760817bc66336e
MrXyfir <mr@xyfir.com>
2016-10-30 11:25:43 -0700

    Improve code. Update validation

--- controllers/publishers/account/update.js
+++ controllers/publishers/account/update.js
@@ -7,7 +7,7 @@
     RETURN
         { error: true, message: string }
     DESCRIPTION
-        Allow publisher's to update their payment method and info
+        Allow publishers to update their payment method and info
 */
 module.exports = function(req, res) {
     
@@ -15,37 +15,52 @@ module.exports = function(req, res) {
 
     // Update payment_info, payment_method
     const update = () => db(cn => {
-        let sql = "UPDATE publishers SET payment_info = ?, payment_method = ? WHERE user_id = ?";
-        let vars = [JSON.stringify(info), req.body.paymentMethod, req.session.uid];
+        let sql = `
+            UPDATE publishers SET
+                payment_info = ?, payment_method = ?
+            WHERE user_id = ?
+        `, vars = [
+            JSON.stringify(info), req.body.paymentMethod,
+            req.session.uid
+        ];
 
         cn.query(sql, vars, (err, result) => {
             cn.release();
 
-            if (err)
-                res.json({ error: true, message: "An unknown error occured" });
-            else
-                res.json({ error: false, message: "Payment info updated successfully" });
+            if (err || !result.affectedRows) {
+                res.json({
+                    error: true, message: "An unknown error occured"
+                });
+            }
+            else {
+                res.json({
+                    error: false, message: "Payment info updated"
+                });
+            }
         });
     });
 
     // Payment via check
     if (req.body.paymentMethod == 1) {
         // Validate info pertaining to check
-        if (!info.name.match(/^([\w-]{2,20}\s?){2,3}$/))
+        if (!info.name)
             res.json({ error: true, message: "Invalid name" });
-        else if (!info.address.match(/^[\w\d -.#,]{5,50}$/))
+        else if (!info.address.match(/^.{5,50}$/))
             res.json({ error: true, message: "Invalid address" });
-        else if (!info.address2.match(/^[\w\d -.#,]{0,50}$/))
+        else if (!info.address2.match(/^.{0,50}$/))
             res.json({ error: true, message: "Invalid address" });
         else if (!String(info.zip).match(/^[0-9]{5}$/))
-            res.json({ error: true, message: "Invalid zip code (US ONLY)" });
-        else if (info.country != "US")
-            res.json({ error: true, message: "Checks are only available for US publishers" });
+            res.json({ error: true, message: "Invalid zip code" });
+        else if (!info.city.match(/^.{0,50}$/))
+            res.json({ error: true, message: "Invalid city" });
+        else if (!info.state.match(/^[\w\s]{0,12}$/))
+            res.json({ error: true, message: "Invalid state" });
         else update();
     }
     // Payment via bank wire
     else if (req.body.paymentMethod == 2) {
         // ** Add validation for bank wire info
+        res.json({ error: true, message: "Payment method unsupported" });
     }
     // Invalid payment method
     else {
