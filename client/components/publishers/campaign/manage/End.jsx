b8c30c006eb2b4f7636c6aca6b1ec77443e52c4d
MrXyfir <mr@xyfir.com>
2016-10-31 10:06:28 -0700

    Improve onConfirm(). Remove this.state.*

--- client/components/publishers/campaign/manage/End.jsx
+++ client/components/publishers/campaign/manage/End.jsx
@@ -11,45 +11,29 @@ export default class EndPublisherCampaign extends React.Component {
 
     constructor(props) {
         super(props);
-
-        this.state = { error: false, message: "", confirm: false };
     }
 
     onConfirm() {
         request({
             url: "api/publishers/campaigns/" + this.props.id,
             method: "DELETE", success: (res) => {
-                res.confirm = true;
-                this.setState(res);
-
-                // Send user back to their campaigns
-                setTimeout(() => {
+                if (!res.error)
                     location.hash = "/publishers/campaigns";
-                }, 7 * 1000);
+                else
+                    swal("Error", res.message, "error");
             }
         });
     }
 
     render() {
-        if (this.state.confirm) {
-            return(
-                <div className="end-campaign">
-                    <Alert type={this.state.error ? "error" : "success"} title={this.state.error ? "Error!" : "Success!"}>
-                        {this.state.message}
-                    </Alert>
-                </div>
-            );
-        }
-        else {
-            return(
-                <div className="end-campaign">
-                    <Alert alert="danger" title="Warning!">
-                        Are you sure you want to end this campaign? Any pending or confirmed earnings in the campaign will be lost!
-                    </Alert>
-                    <Button onClick={() => this.onConfirm()}>End Campaign</Button>
-                </div>
-            );
-        }
+        return(
+            <div className="end-campaign">
+                <Alert alert="danger" title="Warning!">
+                    Are you sure you want to end this campaign? Any pending or confirmed earnings in the campaign will be lost!
+                </Alert>
+                <Button onClick={() => this.onConfirm()}>End Campaign</Button>
+            </div>
+        );
     }
 
 }
