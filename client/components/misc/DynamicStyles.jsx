9bac06e8ce7abfe2629b0c16fbf608d24a65df84
MrXyfir <mr@xyfir.com>
2016-11-13 16:53:28 -0800

    Remove #content margin-bottom

--- client/components/misc/DynamicStyles.jsx
+++ client/components/misc/DynamicStyles.jsx
@@ -12,9 +12,6 @@ export default class DynamicStyles extends React.Component {
                 margin-top: ${
                     document.querySelector("header nav").scrollHeight
                 }px;
-                margin-bottom: ${
-                    document.querySelector("footer").scrollHeight
-                }px;
             }
         `;
     }
