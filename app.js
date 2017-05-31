8ce2e64f725e16f047d52b3fe74466a1dfae682b
MrXyfir <mr@xyfir.com>
2017-05-30 20:28:01 -0700

    Update code style

--- app.js
+++ app.js
@@ -1,77 +1,76 @@
-require("app-module-path").addPath(__dirname);
+require('app-module-path').addPath(__dirname);
 
-const sesStore = require("express-mysql-session");
-const uaParser = require("express-useragent").express;
-const express = require("express");
-const session = require("express-session");
-const parser = require("body-parser");
+const SessionStore = require('express-mysql-session');
+const uaParser = require('express-useragent').express;
+const express = require('express');
+const session = require('express-session');
+const parser = require('body-parser');
 
-const config = require("./config");
-let app = express();
+const config = require('./config');
+const app = express();
 
-if (config.environment.type == "dev") {
-	app.use(require("cors")());
-}
+if (config.environment.type == 'dev') app.use(require('cors')());
 
 /* Serve Static Files */
-app.use("/static", express.static(__dirname + "/static"));
+app.use('/static', express.static(__dirname + '/static'));
 
 /* Body Parser */
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
 /* User Agent Parser */
-app.use("/click", uaParser());
+app.use('/click', uaParser());
 
 /* Sessions */
-const sessionStore = new sesStore({
-    host: config.database.host,
-    port: config.database.port,
-    user: config.database.user,
-    password: config.database.password,
-    database: config.database.database,
-    useConnectionPooling: true
-});
-app.use(session({
+app.use(
+  session({
     secret: config.keys.session,
-    store: sessionStore,
+    store: new SessionStore({
+      host: config.database.host,
+      port: config.database.port,
+      user: config.database.user,
+      password: config.database.password,
+      database: config.database.database,
+      useConnectionPooling: true
+    }),
     saveUninitialized: true,
     resave: true,
     cookie: {
-        httpOnly: false
+      httpOnly: false
     }
-}));
+  })
+);
 
 /* Middleware */
-app.use("/api/panel", (req, res, next) => {
-    if (req.session.uid <= 1000) next();
-    else res.json({ error: true }); 
+app.use('/api/panel', (req, res, next) => {
+  if (req.session.uid <= 1000) next();
+  else res.json({ error: true }); 
 });
-app.use("/api/advertisers", (req, res, next) => {
-    if (req.session.advertiser) next();
-    else res.json({ error: true }); 
+app.use('/api/advertisers', (req, res, next) => {
+  if (req.session.advertiser) next();
+  else res.json({ error: true }); 
 });
-app.use("/api/publishers", (req, res, next) => {
-    if (req.session.publisher) next();
-    else res.json({ error: true }); 
+app.use('/api/publishers', (req, res, next) => {
+  if (req.session.publisher) next();
+  else res.json({ error: true }); 
 });
 
 /* Routes */
-app.use("/", express.static(__dirname + "/public"));
-app.use("/api", require("./controllers/"));
+app.use('/', express.static(__dirname + '/public'));
+app.use('/api', require('./controllers/'));
 
-app.get("/*", (req, res) => {
-    if (config.environment.type == "dev") {
-        req.session.uid = 1;
-        req.session.advertiser = true;
-        req.session.publisher = true;
-    }
-    res.sendFile(__dirname + "/views/App.html");
+app.get('/*', (req, res) => {
+  if (config.environment.type == 'dev') {
+    req.session.uid = 1;
+    req.session.advertiser = true;
+    req.session.publisher = true;
+  }
+  res.sendFile(__dirname + '/views/App.html');
 });
 
 /* Start Cron Jobs */
-if (config.environment.runCronJobs) require("./jobs/start")();
+if (config.environment.runCronJobs) require('./jobs/start')();
 
-app.listen(config.environment.port, () => {
-    console.log("~~Server running on", config.environment.port)
-});
+app.listen(config.environment.port, () =>
+  console.log('~~Server running on', config.environment.port)
+);
